5b3cb89d3285492f3cc13145525cc0e346f99f9d
Wang Zuo <wzuoadjusted@gmail.com>
2017-07-24 18:31:45 +0800

    2.1.1

--- dist/input-number.js
+++ dist/input-number.js
@@ -2,6 +2,14 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var blacklist = require('blacklist');
 var React = require('react');
 var parseNumber = require('./parse');
@@ -10,69 +18,107 @@ var KEY_UP = 38;
 var KEY_DOWN = 40;
 var KEY_ENTER = 13;
 
-module.exports = React.createClass({
-  displayName: 'InputNumber',
+var InputNumber = function (_React$Component) {
+  _inherits(InputNumber, _React$Component);
 
-  getDefaultProps: function getDefaultProps() {
-    return {
-      step: 1
-    };
-  },
-  parse: function parse(val) {
-    return parseNumber(val, this.props.step, this.props.max, this.props.min);
-  },
-  getInitialState: function getInitialState() {
-    return {
-      value: this.parse(this.props.value)
-    };
-  },
-  render: function render() {
-    var props = blacklist(this.props, 'step', 'min', 'max', 'onKeyUp', 'onKeyDown', 'onChange');
+  function InputNumber(props) {
+    _classCallCheck(this, InputNumber);
 
-    return React.createElement('input', _extends({}, props, {
-      type: 'text',
-      value: this.state.value,
-      onKeyUp: this.handleKeyUp,
-      onKeyDown: this.handleKeyDown,
-      onChange: this.handleChange
-    }));
-  },
-  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-    this.setState({
-      value: this.parse(nextProps.value)
-    });
-  },
-  change: function change(value) {
-    if (this.props.onChange) {
-      this.props.onChange(this.parse(value));
-    }
-  },
-  up: function up() {
-    this.change(this.state.value + this.props.step);
-  },
-  down: function down() {
-    this.change(this.state.value - this.props.step);
-  },
-  handleKeyDown: function handleKeyDown(e) {
-    switch (e.keyCode) {
-      case KEY_UP:
-        e.preventDefault();
-        this.up();
-        break;
-      case KEY_DOWN:
-        e.preventDefault();
-        this.down();
-        break;
-    }
-  },
-  handleKeyUp: function handleKeyUp(e) {
-    if (e.keyCode === KEY_ENTER) {
-      this.change(this.state.value);
-    }
-  },
-  handleChange: function handleChange(e) {
-    this.setState({
-      value: e.target.value
-    });
+    var _this = _possibleConstructorReturn(this, (InputNumber.__proto__ || Object.getPrototypeOf(InputNumber)).call(this, props));
+
+    _this.state = {
+      value: _this.parse(props.value)
+    };
+    return _this;
   }
-});
+
+  _createClass(InputNumber, [{
+    key: 'parse',
+    value: function parse(val) {
+      return parseNumber(val, this.props.step, this.props.max, this.props.min);
+    }
+  }, {
+    key: 'componentWillReceiveProps',
+    value: function componentWillReceiveProps(nextProps) {
+      this.setState({
+        value: this.parse(nextProps.value)
+      });
+    }
+  }, {
+    key: 'change',
+    value: function change(value) {
+      if (this.props.onChange) {
+        this.props.onChange(this.parse(value));
+      }
+    }
+  }, {
+    key: 'up',
+    value: function up() {
+      this.change(this.state.value + this.props.step);
+    }
+  }, {
+    key: 'down',
+    value: function down() {
+      this.change(this.state.value - this.props.step);
+    }
+  }, {
+    key: 'handleKeyDown',
+    value: function handleKeyDown(e) {
+      switch (e.keyCode) {
+        case KEY_UP:
+          e.preventDefault();
+          this.up();
+          break;
+        case KEY_DOWN:
+          e.preventDefault();
+          this.down();
+          break;
+      }
+    }
+  }, {
+    key: 'handleKeyUp',
+    value: function handleKeyUp(e) {
+      if (e.keyCode === KEY_ENTER) {
+        this.change(this.state.value);
+      }
+    }
+  }, {
+    key: 'handleChange',
+    value: function handleChange(e) {
+      this.setState({
+        value: e.target.value
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      var props = blacklist(this.props, 'step', 'min', 'max', 'onKeyUp', 'onKeyDown', 'onChange');
+      var value = this.state.value;
+
+
+      return React.createElement('input', _extends({}, props, {
+        type: 'text',
+        value: value,
+        onKeyUp: function onKeyUp(e) {
+          return _this2.handleKeyUp(e);
+        },
+        onKeyDown: function onKeyDown(e) {
+          return _this2.handleKeyDown(e);
+        },
+        onChange: function onChange(e) {
+          return _this2.handleChange(e);
+        }
+      }));
+    }
+  }]);
+
+  return InputNumber;
+}(React.Component);
+
+InputNumber.defaultProps = {
+  step: 1
+};
+
+module.exports = InputNumber;
--- example/app.js
+++ example/app.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const InputNumber = require('../lib/input-number.js')
+const InputNumber = require('../lib/input-number.js');
+const packageJSON = require('../package.json');
 
 class App extends React.Component {
   constructor(props) {
@@ -13,31 +14,40 @@ class App extends React.Component {
 
   _onChange(value) {
     console.log('onChange value', value);
-    this.setState({number: value});
+    this.setState({ number: value });
   }
 
   _onInputChange(e) {
-    this.setState({number: e.target.value});
+    this.setState({ number: e.target.value });
   }
 
   render() {
     return (
       <div>
+        <h1>
+          {packageJSON.name} {packageJSON.version}
+        </h1>
         <InputNumber
           className="input"
-          min={10} max={100} step={0.03}
+          min={10}
+          max={100}
+          step={0.03}
           value={this.state.number}
-          onChange={value => this._onChange(value)} />
-        <br/>
+          onChange={value => this._onChange(value)}
+        />
+        <br />
         <input
           className="input"
           type="number"
-          min="10" max="100" step="0.03"
+          min="10"
+          max="100"
+          step="0.03"
           value={this.state.number}
-          onChange={(e) => this._onInputChange(e)} />
+          onChange={e => this._onInputChange(e)}
+        />
       </div>
     );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
