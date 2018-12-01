1cf99ab587e53cbb54d5fad9860f51029a5111d9
Wang Zuo <wzuoadjusted@gmail.com>
2018-12-01 10:19:54 +0800

    sync

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: [['@babel/env', { loose: true }], '@babel/preset-react'],
+  presets: [['@babel/preset-env', { loose: true }], '@babel/preset-react'],
   plugins: [
     ['@babel/proposal-class-properties', { loose: true }],
     '@babel/plugin-proposal-export-default-from'
